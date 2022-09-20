8ebf391aa7e147ffe3c30e9a1155b40bb2185bba
Shuvrajeet Das <shuvrajeet17@gmail.com>
2022-09-20 20:54:50 +0530

    neat updates

---
+++ NeatPython/neat_trial.py
@@ -0,0 +1,81 @@
+from neat import config, population, genome, reproduction, species, stagnation, nn, reporting, statistics, checkpoint
+import neat
+import numpy as np
+import gym
+
+render = True
+render2 = True
+
+highest_scores = []
+
+
+def eval_genomes(genomes, configure):
+    global highest_scores
+
+    member = 0
+    high_score = 0
+    env = gym.make('CartPole-v1')
+    env._max_episode_steps = 100000
+    for genome_id, genome in genomes:
+        print('Member:', member)
+        score = 0
+        observation = env.reset()
+        done = False
+        net = neat.nn.FeedForwardNetwork.create(genome, configure)
+
+        while not done:
+            inputs = [observation[0], observation[1],
+                      observation[2], observation[3]]
+            if render:
+                env.render()
+            action = round(net.activate(inputs)[0])
+            observation, reward, done, info = env.step(action)
+            if done:
+                break
+            else:
+                score += 1
+
+        genome.fitness = score
+        if score > high_score:
+            high_score = score
+
+        member += 1
+        print('Fitness:', score)
+        print('Highest_Scores_Over_Generation:', highest_scores)
+        print('------------------------------')
+    env.close()
+    highest_scores.append(high_score)
+
+
+def render_winner(net):
+    print('Rendering Winner')
+
+    env = gym.make('CartPole-v0')
+    observation = env.reset()
+    done = False
+
+    while not done:
+        action = round(net.activate(
+            [observation[0], observation[1], observation[2], observation[3]])[0])
+        env.render()
+        observation, reward, done, info = env.step(action)
+        if done:
+            break
+    env.close()
+    print('---------------------------------')
+
+
+if __name__ == '__main__':
+    configure = neat.config.Config(neat.genome.DefaultGenome, neat.reproduction.DefaultReproduction,
+                     neat.species.DefaultSpeciesSet, neat.stagnation.DefaultStagnation,
+                     'config-feedforward')
+
+    p = neat.population.Population(configure)
+    p.add_reporter(neat.reporting.StdOutReporter(True))
+    stats = neat.statistics.StatisticsReporter()
+    p.add_reporter(stats)
+    winner = p.run(eval_genomes)
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, configure)
+    if render2:
+        render_winner(winner_net)
+    input("Enter to Quit: ")
---
+++ NeatPython/params.py
@@ -0,0 +1,6 @@
+max_steps = 1000
+episodes = 1
+render = True
+generations = 50
+checkpoint = './ckpt/'
+numCores = 4
