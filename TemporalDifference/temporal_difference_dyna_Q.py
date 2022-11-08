4a5e9533be7a2635a15c93059b03f7fee27fcdb1
Shuvrajeet Das <shuvrajeet17@gmail.com>
2022-11-08 18:01:37 +0530

    updates

--- TemporalDifference/temporal_difference_dyna_Q.py
+++ TemporalDifference/temporal_difference_dyna_Q.py
@@ -26,6 +26,8 @@ np.set_printoptions(formatter={'int': color_sign})
 
 class EnvMaze:
     def __init__(self, env_id='RandomMaze-v0'):
+        self._maze = random_maze(
+            width=9, height=10, complexity=.75, density=.75)
         self.reset()
 
     def render(self, if_plot=False):
@@ -34,8 +36,7 @@ class EnvMaze:
         print()
 
     def reset(self):
-        self.maze = random_maze(
-            width=9, height=10, complexity=.75, density=.75)
+        self.maze = self._maze.copy()
         self.shapes = self.maze.shape
         self.movements = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         self.start = [1, 1]
@@ -45,17 +46,17 @@ class EnvMaze:
         self.current = self.start
         self.maze[self.current[0]][self.current[1]] = 8
         self.done = False
-        return self.current[0]*self.maze.shape[0] + self.current[1]
+        return self.current[0]*self.maze.shape[1] + self.current[1]
 
     def step(self, action):
-        if (self.maze[self.current[0] + self.movements[action][0]][self.current[1] + self.movements[action][1]]) != 1:
+        if not (self.maze[self.current[0] + self.movements[action][0]][self.current[1] + self.movements[action][1]]):
             self.maze[self.current[0]][self.current[1]] = 0
             self.current[0] = np.clip(
                 self.current[0] + self.movements[action][0], 1, self.shapes[0]-2)
             self.current[1] = np.clip(
-                self.current[1] + self.movements[action][1], 1, self.shapes[1]-2);print(self.current,self.movements[action],self.maze[10][7])
+                self.current[1] + self.movements[action][1], 1, self.shapes[1]-2)
         self.maze[self.current[0]][self.current[1]] = 8
-        state = self.current[0]*self.maze.shape[0] + self.current[1]
+        state = self.current[0]*self.maze.shape[1] + self.current[1]
         if self.current == self.end:
             reward = 10
             self.done = True
@@ -71,7 +72,7 @@ class Model():
         self.rewards = np.zeros((n_states, n_actions))
 
     def add(self, s, a, s_prime, r):
-        print(s, a, s_prime, r)
+        # print(s, a, s_prime, r)
         self.transitions[s, a] = s_prime
         self.rewards[s, a] = r
 
@@ -100,10 +101,17 @@ def dyna_Q_learning(env, iterations=1000):
     epsilon = 1
     decay = 1/iterations
     multi_step = 100
+    minm_actions = np.inf
+    maxm_actions = 0
+    num_actions_per_ep = []
+    avg_actions_per_ep = []
+    minm_actions_per_ep = []
+    maxm_actions_per_ep = []
 
     for iter in tqdm.tqdm(range(iterations)):
         old_state = env.reset()
         done = False
+        actions_taken = 0
         while not done:
             if np.random.randn() <= epsilon:
                 action = np.random.randint(0, env.n_actions)
@@ -115,7 +123,7 @@ def dyna_Q_learning(env, iterations=1000):
                 (reward + gamma*np.max([Q[state][a]
                                         for a in range(env.n_actions)]) - Q[old_state][action])
             model.add(old_state, action, state, reward)
-            env.render()
+            # env.render()
             multi_step = int(multi_step*0.95)
             for i in range(multi_step):
                 s, a = model.sample()
@@ -124,8 +132,23 @@ def dyna_Q_learning(env, iterations=1000):
             policy[old_state] = np.argmax(
                 [Q[old_state][a] for a in range(env.n_actions)])
             old_state = state
+            actions_taken += 1
+        num_actions_per_ep.append(actions_taken)
+        avg_actions_per_ep.append(np.mean(num_actions_per_ep))
+        minm_actions = min(minm_actions, actions_taken)
+        maxm_actions = max(maxm_actions, actions_taken)
+        minm_actions_per_ep.append(minm_actions)
+        maxm_actions_per_ep.append(maxm_actions)
         epsilon -= decay
 
+    plt.plot(num_actions_per_ep, label='Nums')
+    plt.plot(avg_actions_per_ep, label='Avg')
+    plt.plot(minm_actions_per_ep, label='Min')
+    plt.plot(maxm_actions_per_ep, label='Max')
+    plt.legend()
+    plt.xlabel('Episodes')
+    plt.ylabel('Num Actions')
+    plt.show()
     return Q, policy
 
 
@@ -150,5 +173,8 @@ def games_trial(env, policy, no_of_games=100):
 if __name__ == '__main__':
     env = EnvMaze()
     print(env.reset())
+    env.render()
     Q, policy = dyna_Q_learning(env)
+    env.reset()
+    env.render()
     games_trial(env, policy, no_of_games=100)
