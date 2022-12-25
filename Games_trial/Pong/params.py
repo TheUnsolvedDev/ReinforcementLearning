27f4d6b23ef22434dcc7049c487aee761548a0ef
TheUnsolvedDev <shuvrajeet17@gmail.com>
2022-12-25 16:10:36 +0530

    updates

--- Games_trial/Pong/params.py
+++ Games_trial/Pong/params.py
@@ -1,5 +1,6 @@
 import gym
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 EPSILON_LEARNING_START = 1
 EPSILON_LEARNING_END = 0.01
@@ -20,6 +21,10 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def preprocess(state):
     state = state[USEFUL_REGION_TOP: USEFUL_REGION_BOTTOM, :, :]
+    state = state[::2, ::2, :]
+    state[state == 144] = 0
+    state[state == 109] = 0
+    state[state != 0] = 1
     state = tf.image.resize(state, IMG_SIZE)
     return tf.expand_dims(tf.reduce_mean(state, axis=-1), axis=-1)
 
